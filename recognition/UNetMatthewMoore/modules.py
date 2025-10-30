8714334939c142680e7a50728c5aa837037ce6fd
Matthew Moore <Matthew.r.moore05@gmail.com>
2025-10-30 11:49:45 +1000

    Wrote down requirements for each file in the assignment

---
+++ recognition/UNetMatthewMoore/modules.py
@@ -0,0 +1,2 @@
+# Source code of the components of the model.
+# Each component must be implemented as a class or function.
---
+++ recognition/UNetMatthewMoore/predict.py
@@ -0,0 +1,1 @@
+#Shows example usage of the trained model. Print out any results and/or provide visualisations where applicable
